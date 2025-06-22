0adc18b34c35cad29c8db09d92ea0704f9eb1726
ArielAlzogarayFlores <arotuoficial@gmail.com>
2025-06-22 14:49:01 -0300

    escenario 2, ejercicio1

--- TP/TP-grupo5.R
+++ TP/TP-grupo5.R
@@ -4,8 +4,8 @@
 
 # Creamos una muestra del peso total de los pasajeros en 1000 viajes de avión
 
-muestra <- numeric(1000)
-for (i in 1:1000) { # Cada dato en la muestra es la suma de los pesos de 81 pasajeros adultos, es decir, el peso total en un vuelo
+muestra <- numeric(10000)
+for (i in 1:10000) { # Cada dato en la muestra es la suma de los pesos de 81 pasajeros adultos, es decir, el peso total en un vuelo
   muestra[i] <- sum(rnorm(81, mean=70, sd = 7))  
 }
 
@@ -15,5 +15,21 @@ cantRendNoOptimo <- sum(muestra>5500)
 
 # Calculamos la probabilidad
 
+probaRendNoOptimo <- cantRendNoOptimo/length(muestra)
+
+# Escenario 2, ejercicio 1
+
+muestra <- numeric(100000)
+for (i in 1:100000){
+  # 1 corresponde a adultos y 0 a niños
+  pasajeros <- rbinom(n=81, size=1, prob=0.95)
+  adultos <- sum(pasajeros == 1)
+  niños <- sum(pasajeros == 0)
+  pesoAdultos <- sum(rnorm(adultos, mean=70, sd=7))
+  pesoNiños <- sum(rnorm(niños, mean=20, sd=5))
+  muestra[i] <- pesoAdultos + pesoNiños
+}
+
+cantRendNoOptimo <- sum(muestra > 5500)
 probaRendNoOptimo <- cantRendNoOptimo/length(muestra)
 
